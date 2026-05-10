5b42131d556c328700ff0ca470ba06abf7c36c0d
kasturichavan1 <kasturi.chavan07@gmail.com>
2026-05-10 20:02:12 +0530

    Update three_distinct_digits.c

--- If Else and Boolean Logic/three_distinct_digits.c
+++ If Else and Boolean Logic/three_distinct_digits.c
@@ -1,1 +1,22 @@
+Q. For a 3 digit number, check if all the digits are distinct.
 
+
+#include <stdio.h>
+
+int main() {
+    int a, f, m, l;
+
+    printf("Enter a 3 digit number: ");
+    scanf("%d", &a);
+
+    l = a % 10;        
+    m = (a / 10) % 10;  
+    f = a / 100;        
+
+    if (f != m && m != l && f != l)
+        printf("All digits are unique");
+    else
+        printf("Digit repeated");
+
+    return 0;
+}
